545681ed5212ed495a870eb7a18514de341120fa
Bruno Machado <brunomdr46@gmail.com>
2021-09-27 17:28:26 -0300

    Node.js

---
+++ Node.js/index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var HeroiAleatorio = superheroes.random();
+console.log(HeroiAleatorio)
+
+var VilaoAleatorio = supervillains.random();
+console.log(VilaoAleatorio)
