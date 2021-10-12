7173c0aca9eadd0a6c58382a29f06e75ca491204
Bruno Machado <brunomdr46@gmail.com>
2021-10-12 17:26:13 -0300

    API da TI

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -5,22 +5,115 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
 const articleSchema = {
-    title: String,
-    content: String
+  title: String,
+  content: String,
 };
 
 const Article = mongoose.model("Article", articleSchema);
 
-//TODO
+app.route("/articles")
 
-app.listen(3000, function() {
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .post(function (req, res) {
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Artigo adicionado com sucesso!");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Todos artigos foram deletados com sucesso!");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+  // Artigo específico
+
+  app.route("/articles/:articleTitle")
+
+  .get(function(req, res){
+
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticles){
+      if (foundArticles) {
+        res.send(foundArticles);
+      } else {
+        res.send("Não foram encontrados artigos com esse título.")
+      }
+    });
+  })
+
+  .put(function(req, res){
+
+    Article.replaceOne(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      function(err){
+        if (!err) {
+          console.log("Artigo atualizado com sucesso.")
+        }
+      }
+    );
+  })
+
+  .patch(function(req, res){
+
+    Article.updateOne(
+      {title: req.params.articleTitle},
+      {$set: req.body},
+      function(err) {
+        if (!err) {
+          res.send('Artigo atualizado com sucesso.');
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+
+  .delete(function(req, res){
+    Article.deleteOne(
+      {title: req.params.articleTitle},
+      function(err) {
+        if (!err) {
+          res.send('Artigo atualizado com sucesso.');
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
