ce167540d78308349f58558a2a5f4d59596a670a
Bruno Machado <brunomdr46@gmail.com>
2021-10-19 22:57:59 -0300

    Enciclopédia de Emojis

---
+++ Emojipedia/src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Card";
+import emojipedia from "../emojipedia";
+
+function criarCard(emojipedia) {
+  return (
+    <Card
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(criarCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ Emojipedia/src/emojipedia.js
@@ -0,0 +1,60 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Bíceps flexionado",
+    meaning:
+      "'Você pode fazer isso!' ou 'Sinto-me forte!' Arme-se com bíceps flexionados. Também usado em conexão com a prática de esportes, por ex. na Academia."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Mãos juntas",
+    meaning:
+      "Duas mãos pressionadas uma contra a outra. Atualmente é muito introvertido, fazendo uma oração ou esperando a iluminação. Também é usado como “high five” ou para dizer obrigado."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolando no chão de rir",
+    meaning:
+      "Isso é engraçado! Um rosto sorridente, rolando no chão, rindo. O rosto está rindo sem limites. A versão emoji de “ROFL“. Significa rolando no chão de tanto de rir (rolling on the floor laughing)“."
+  },
+  {
+    id: 4,
+    emoji: "❤️",
+    name: "Coração vermelho",
+    meaning:
+      "Um emoji clássico de coração de amor vermelho, usado para expressões de amor e romance. Este é o emoji de coração mais popular. Existe um emoji semelhante para o naipe de coração em um baralho de cartas."
+  },
+  {
+    id: 5,
+    emoji: "✨",
+    name: "Brilhos",
+    meaning:
+      "Normalmente usado para indicar vários sentimentos positivos, incluindo amor, felicidade, beleza, gratidão e entusiasmo, bem como novidade ou limpeza. Também pode ser usado como uma forma de ✨enfatizar✨ ou para transmitir tons sarcásticos."
+  },
+  {
+    id: 6,
+    emoji: "🔥",
+    name: "Fogo",
+    meaning:
+      "Comumente usado para várias expressões metafóricas relacionadas ao fogo, incluindo a gíria quente (“atraente”) e iluminada (“excelente”). O Snapchat exibe 🔥 ao lado de dois usuários que estão em um snapstreak ou que tenham enviado mensagens por mais de três dias consecutivos."
+  },
+  {
+    id: 7,
+    emoji: "💕",
+    name: "Dois corações",
+    meaning:
+      "Dois corações rosa de amor. Um maior que o outro. Pode ser usado para mostrar que “o amor está no ar”. No Snapchat, este emoji próximo a um contato indica que vocês foram os melhores amigos por dois meses consecutivos"
+  },
+  {
+    id: 8,
+    emoji: "😷",
+    name: "Rosto com máscara médica",
+    meaning:
+      "Um rosto amarelo com uma máscara cirúrgica branca, usada por profissionais de saúde em hospitais e também pelo público em geral durante uma pandemia. Pode ser usado para evitar adoecimento ou propagação de doenças transmitidas pelo ar. Em 2020, este emoji passou a ser usado principalmente em referência ao Coronavirus / COVID-19."
+  },
+];
+
+export default emojipedia;
